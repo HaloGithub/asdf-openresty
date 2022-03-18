85b36ab2a999d0000f0583c4c8ff06eb0884de52
Lek <28960001+HaloGithub@users.noreply.github.com>
2022-03-19 00:25:30 +0800

    Bugfix: Install coreutils for MacOS v2.

--- bin/_os.bash
+++ bin/_os.bash
@@ -7,20 +7,15 @@ fail() {
 }
 
 
-realpath_cmd() {
-    uname=$(uname);
-    case "$uname" in
-    (*Linux*)
-        realpath_cmd="realpath"
-        ;;
-    (*Darwin*)
-        brew install coreutils
-        realpath_cmd="grealpath"
-        ;;
-    (*)
-        fail "Unsupported platform: $uname"
-        exit 2
-        ;;
-    esac;
-    echo "$realpath_cmd"
-}
+uname=$(uname);
+case "$uname" in
+(*Linux*)
+    ;;
+(*Darwin*)
+    brew install coreutils
+    ;;
+(*)
+    fail "Unsupported platform: $uname"
+    exit 2
+    ;;
+esac;
