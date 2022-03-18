e717d37269ff6ae953dad6bc87206ce439ab24b0
Lek <28960001+HaloGithub@users.noreply.github.com>
2022-03-18 20:30:09 +0800

    Fix shellcheck error 2.

--- lib/utils.bash
+++ lib/utils.bash
@@ -53,26 +53,26 @@ list_all_versions() {
 
 download_openssl() {
     download_path=$1
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path" \
-    && curl -fSL "$RESTY_OPENSSL_URL_BASE/openssl-$RESTY_OPENSSL_VERSION.tar.gz" -o "openssl-$RESTY_OPENSSL_VERSION.tar.gz" \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path"
+    curl -fSL "$RESTY_OPENSSL_URL_BASE/openssl-$RESTY_OPENSSL_VERSION.tar.gz" -o "openssl-$RESTY_OPENSSL_VERSION.tar.gz" \
     && tar xzf "openssl-$RESTY_OPENSSL_VERSION.tar.gz"
 }
 
 
 install_openssl() {
-    download_path=$1 \
-    install_path=$2 \
+    download_path=$1
+    install_path=$2
     make_j=$3
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path/openssl-$RESTY_OPENSSL_VERSION" \
-    && if [ "$(echo $RESTY_OPENSSL_VERSION | cut -c 1-5)" = "1.1.1" ] ; then \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path/openssl-$RESTY_OPENSSL_VERSION"
+    if [ "$(echo "$RESTY_OPENSSL_VERSION" | cut -c 1-5)" = "1.1.1" ] ; then \
         echo 'patching OpenSSL 1.1.1 for OpenResty' \
         && curl -s "https://raw.githubusercontent.com/openresty/openresty/master/patches/openssl-$RESTY_OPENSSL_PATCH_VERSION-sess_set_get_cb_yield.patch" | patch -p1 ; \
     fi \
-    && if [ "$(echo $RESTY_OPENSSL_VERSION | cut -c 1-5)" = "1.1.0" ] ; then \
+    && if [ "$(echo "$RESTY_OPENSSL_VERSION" | cut -c 1-5)" = "1.1.0" ] ; then \
         echo 'patching OpenSSL 1.1.0 for OpenResty' \
         && curl -s https://raw.githubusercontent.com/openresty/openresty/ed328977028c3ec3033bc25873ee360056e247cd/patches/openssl-1.1.0j-parallel_build_fix.patch | patch -p1 \
         && curl -s "https://raw.githubusercontent.com/openresty/openresty/master/patches/openssl-$RESTY_OPENSSL_PATCH_VERSION-sess_set_get_cb_yield.patch" | patch -p1 ; \
@@ -90,22 +90,22 @@ install_openssl() {
 
 download_pcre() {
     download_path=$1
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path" \
-    && curl -fSL "https://downloads.sourceforge.net/project/pcre/pcre/$RESTY_PCRE_VERSION/pcre-$RESTY_PCRE_VERSION.tar.gz" -o "pcre-$RESTY_PCRE_VERSION.tar.gz" \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path"
+    curl -fSL "https://downloads.sourceforge.net/project/pcre/pcre/$RESTY_PCRE_VERSION/pcre-$RESTY_PCRE_VERSION.tar.gz" -o "pcre-$RESTY_PCRE_VERSION.tar.gz" \
     && echo "$RESTY_PCRE_SHA256  pcre-$RESTY_PCRE_VERSION.tar.gz" | shasum -a 256 --check \
-    && tar xzf pcre-$RESTY_PCRE_VERSION.tar.gz
+    && tar xzf "pcre-$RESTY_PCRE_VERSION.tar.gz"
 }
 
 
 install_pcre() {
-    download_path=$1 \
-    install_path=$2 \
+    download_path=$1
+    install_path=$2
     make_j=$3
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path/pcre-$RESTY_PCRE_VERSION" \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path/pcre-$RESTY_PCRE_VERSION" \
     && ./configure \
         --prefix="$install_path/pcre" \
         --disable-cpp \
@@ -118,24 +118,24 @@ install_pcre() {
 
 
 download_openresty() {
-    download_path=$1 \
+    download_path=$1
     install_version=$2
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path" \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path" \
     && curl -fSL "https://openresty.org/download/openresty-$install_version.tar.gz" -o "openresty-$install_version.tar.gz" \
     && tar xzf "openresty-$install_version.tar.gz"
 }
 
 
 install_openresty() {
-    download_path=$1 \
-    install_path=$2 \
-    make_j=$3 \
+    download_path=$1
+    install_path=$2
+    make_j=$3
     install_version=$4
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path/openresty-$install_version" \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path/openresty-$install_version" \
     && eval ./configure -j"$make_j" \
         "$_RESTY_CONFIG_DEPS" \
         "$RESTY_CONFIG_OPTIONS" \
@@ -151,21 +151,21 @@ install_openresty() {
 
 download_luarocks() {
     download_path=$1
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path" \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path" \
     && curl -fSL "https://luarocks.github.io/luarocks/releases/luarocks-$RESTY_LUAROCKS_VERSION.tar.gz" -o "luarocks-$RESTY_LUAROCKS_VERSION.tar.gz" \
     && tar xzf "luarocks-$RESTY_LUAROCKS_VERSION.tar.gz"
 }
 
 
 install_luarocks() {
-    download_path=$1 \
-    install_path=$2 \
+    download_path=$1
+    install_path=$2
     make_j=$3
-    # shellcheck source=lib/vars.bash
-    source "$plugin_dir/lib/vars.bash" \
-    && cd "$download_path/luarocks-$RESTY_LUAROCKS_VERSION" \
+    # shellcheck source=./lib/vars.bash
+    source "$plugin_dir/lib/vars.bash"
+    cd "$download_path/luarocks-$RESTY_LUAROCKS_VERSION" \
     && ./configure \
         --prefix="$install_path/luarocks" \
         --with-lua="$install_path/openresty/luajit" \
