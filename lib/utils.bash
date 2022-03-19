4da75a28f377fcdb0125a2274fb62b32b4200268
Lek <28960001+HaloGithub@users.noreply.github.com>
2022-03-19 14:48:50 +0800

    Bugfix shellcheck error.

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,15 +3,6 @@
 set -euo pipefail
 
 
-if [ "${#BASH_SOURCE[@]}" -gt 0 ];
-then
-    current_script_path="${BASH_SOURCE[0]}"
-else
-    current_script_path="$0"
-fi
-current_script_dir="$(dirname "$current_script_path")"
-
-
 sort_versions() {
     sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
         LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
